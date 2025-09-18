cee2e5c2a3fb028dc56af607da32cbfe694071bd
vidya <159389388+Vidyasravanthigarikapati@users.noreply.github.com>
2025-09-17 22:15:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+require([
+  "esri/WebScene",
+  "esri/views/SceneView",
+  "esri/widgets/Legend",
+  "esri/widgets/LayerList",
+  "esri/widgets/Search"
+], function(WebScene, SceneView, Legend, LayerList, Search) {
+
+  // Web Scene IDs
+  const chicagoSceneId = "fef6868cafcc420eb4bc2e78d54c38d7";
+  const bostonSceneId = "8046207c1c214b5587230f5e5f8efc77";
+
+  // Create WebScenes
+  const chicagoScene = new WebScene({ portalItem: { id: chicagoSceneId } });
+  const bostonScene = new WebScene({ portalItem: { id: bostonSceneId } });
+
+  // Initialize SceneView 
+  const view = new SceneView({
+    container: "viewDiv",
+    map: chicagoScene,
+    camera: {
+      position: { x: -87.6298, y: 41.8781, z: 800 },
+      tilt: 75,
+      heading: 30
+    }
+  });
+
+  // Add widgets
+  const legend = new Legend({ view });
+  const layerList = new LayerList({ view });
+  const searchWidget = new Search({ view });
+
+  view.ui.add(legend, "bottom-left");
+  view.ui.add(layerList, "top-left");
+  view.ui.add(searchWidget, "top-left");
+
+  // --- Button Logic ---
+  document.getElementById("chicagoBtn").addEventListener("click", () => {
+    view.map = chicagoScene;
+    view.camera.position = { x: -87.6298, y: 41.8781, z: 800 };
+    view.camera.tilt = 75;
+    view.camera.heading = 30;
+  });
+
+  document.getElementById("bostonBtn").addEventListener("click", () => {
+    view.map = bostonScene;
+    view.camera.position = { x: -71.0589, y: 42.3601, z: 800 };
+    view.camera.tilt = 75;
+    view.camera.heading = 30;
+  });
+
+});
